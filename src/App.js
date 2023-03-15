f1c0b63031fd065043f57c78204fea7de1122a80
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2023-03-15 07:21:52 +0600

    login register  page added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
 import Navbar from './Components/Navbar';
+import Register from './Components/Register/Register';
 import Footer from './Components/Shared/Footer/Footer';
 import Tutors from './Components/Tutors/Tutors';
 
@@ -15,6 +17,8 @@ function App() {
         
         <Route path="/" element={<Home/>}></Route>
         <Route path="/tutors" element={<Tutors/>}></Route>
+        <Route path="login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
 
       </Routes>
       <Footer/>
